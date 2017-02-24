8f07c3c105b53c6213573899f05a3c13b230e954
Ettore Forigo <etty.2000.ef@gmail.com>
2017-02-24 13:14:03 +0100

    Database models created

--- scoreboard/admin.py
+++ scoreboard/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Sport
+from .models import Sport, Team, Event
 
 admin.site.register(Sport)
+admin.site.register(Team)
+admin.site.register(Event)
--- scoreboard/models.py
+++ scoreboard/models.py
@@ -6,3 +6,19 @@ class Sport(models.Model):
 
 	def __str__(self):
 		return self.name
+
+
+class Team(models.Model):
+	sport = models.ForeignKey(Sport, on_delete=models.CASCADE)
+	name = models.CharField(max_length=20)
+	score = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.name
+
+
+class Event(models.Model):
+	sport = models.ForeignKey(Sport, on_delete=models.CASCADE)
+	match_date = models.DateTimeField('match date')
+	team0 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='fk_team0')
+	team1 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='fk_team1')
