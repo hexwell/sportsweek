4b63363b5af3f20698049d6f2b8ca80b6b7681ae
Ettore Forigo <etty.2000.ef@gmail.com>
2017-02-25 01:34:11 +0100

    Implemented Ranking view for each sport

--- scoreboard/views.py
+++ scoreboard/views.py
@@ -1,9 +1,17 @@
+from django.shortcuts import render
 from django.views import generic
 
-from .models import Sport
+from .models import Sport, Team
 
 
 class IndexView(generic.ListView):
 	model = Sport
 	context_object_name = 'sports'
 	template_name = 'scoreboard/index.html'
+
+
+def ranking_view(request, sport_name):
+	context = {
+		'teams': Team.objects.filter(sport=Sport.objects.filter(name=sport_name)).order_by('-score')
+	}
+	return render(request, 'scoreboard/ranking.html', context)
