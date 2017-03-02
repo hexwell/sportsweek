a7c91c560ce6632ca312e226719e19c25d759a95
Ettore Forigo <etty.2000.ef@gmail.com>
2017-03-03 00:26:09 +0100

    Added Events Visualization

--- scoreboard/models.py
+++ scoreboard/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 class Sport(models.Model):
@@ -19,6 +22,18 @@ class Team(models.Model):
 
 class Event(models.Model):
 	sport = models.ForeignKey(Sport, on_delete=models.CASCADE)
-	match_date = models.DateTimeField('match date')
 	team0 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='fk_team0')
 	team1 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='fk_team1')
+	match_date = models.DateTimeField('match date')
+	expired = False
+
+	def is_imminent(self, interval):
+		now = timezone.now()
+		delta = self.match_date - now
+		if delta <= datetime.timedelta():
+			delta = now - self.match_date
+			return delta <= datetime.timedelta(hours=3)
+		return delta <= interval
+
+	def __str__(self):
+		return self.team0.name + ' VS ' + self.team1.name
