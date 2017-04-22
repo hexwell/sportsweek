de92d52b9c3295a4e6e0a42c992dac8dd93f9775
Ettore Forigo <etty.2000.ef@gmail.com>
2017-04-22 18:51:22 +0200

    Sport forms completely working

--- scoreboard/urls.py
+++ scoreboard/urls.py
@@ -4,8 +4,11 @@ from . import views
 
 app_name = 'scoreboard'
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<sport_id>[0-9]+)/$', views.RankingList.as_view(), name='ranking'),
-    url(r'^getupdatedscores/$', views.get_updated_scores, name="get_updated_scores"),
-    url(r'^create/sport/$', views.SportFormView.as_view(), name="create_sport")
+	url(r'^getupdatedscores/$', views.get_updated_scores, name="get_updated_scores"),
+	url(r'^$', views.IndexView.as_view(), name='index'),
+	url(r'^(?P<sport_id>[0-9]+)/$', views.RankingList.as_view(), name='ranking'),
+	url(r'^(?P<sport_id>[0-9]+)/create/event/$', views.CreateSportView.as_view(), name="create_event"),
+	url(r'^create/sport/$', views.CreateSportView.as_view(), name="create_sport"),
+	url(r'^(?P<sport_id>[0-9]+)/update/$', views.UpdateSportView.as_view(), name="update_sport"),
+	url(r'^(?P<sport_id>[0-9]+)/delete/$', views.DeleteSportView.as_view(), name="delete_sport"),
 ]
